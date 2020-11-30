0d7e2b97a588adf8642552abfd149769e54739e6
Anthony9698 <anthonyviera4@gmail.com>
2020-11-30 13:33:28 -0600

    Updated navigation links to utilize NavLink component.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
           <Checkout /> */}
           <Switch>
             <Route path="/checkout" component={Checkout} />
-            <Route path="orders" component={Order} />
+            <Route path="/orders" component={Orders} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>

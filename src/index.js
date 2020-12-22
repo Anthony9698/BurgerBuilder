aa0417766a986298b486daa8b785256b561e8e66
Anthony9698 <anthonyviera4@gmail.com>
2020-12-22 14:23:29 -0600

    Added a functional logout link for authenticated users.

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import thunk from 'redux-thunk';
 import authReducer from './store/reducers/auth';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
     order: orderReducer,
